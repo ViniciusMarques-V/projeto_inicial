46040bae5e4a901010aa22dc72f9b1d1de887dad
unknown <vinicius.marqques@gmail.com>
2024-08-06 12:23:24 -0300

    Meu primeiro Comit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+
+# Variaveis 
+
+idade = 32
+nome = "Vinicius"
+
+# Print 
+print(idade)
+
+# Soma
+nova_idade =  idade +1
+print(nova_idade)
+
+#lista 
+lista = [1,2,3,4,5]
+
+# Metodos
+lista_invertida = lista.reverse()
